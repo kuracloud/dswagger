cd18c4c941ab6aee6e5a1d6e85e7ebbbdb08bdff
root <root@docker-swagger.adi.co.nz>
2016-10-05 10:11:09 +1300

    Minor changes to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM nginx
+MAINTAINER drunner
 
 RUN apt-get update \
  && apt-get upgrade -y \
@@ -14,3 +15,9 @@ RUN mv /usr/share/nginx/html/index.html /usr/share/nginx/html/swagger.html
 
 COPY ["./swaggerfiles","/usr/share/nginx/html/"]
 
+# add in the assets.
+COPY ["./drunner","/drunner"]
+RUN chmod a-w -R /drunner
+
+# lock in druser.
+USER swagger
---
+++ drunner/service.lua
@@ -0,0 +1,53 @@
+-- drunner service configuration for minimalexample
+
+function drunner_setup()
+addconfig("PORT","The port to run Swagger on","80","port",true,true)
+end
+
+-- everything past here are functions that can be run from the commandline,
+-- e.g. minimalexample help
+
+function start()
+   print(dsub("Launching Swagger on port ${PORT}"))
+
+   dconfig_set("RUNNING","true")
+
+   if (drunning(containername)) then
+      print("Swagger is already running.")
+   else
+      result=drun("docker", "run", "-d",
+      "-p", "${PORT}:80",
+      "-p", "${PORT}:80/udp",
+      "--restart=always",
+      "--name", containername,
+      "${IMAGENAME}")
+
+      if result~=0 then
+        print("Failed to start Swagger.")
+      end
+   end
+end
+
+function stop()
+  dconfig_set("RUNNING","false")
+  dstop(containername)
+end
+
+function info()
+   if (drunning(containername)) then
+      print(dsub("Swagger is running on port ${PORT}."))
+   else
+      print("Swagger is not currently running.")
+   end
+end
+
+function help()
+   return [[
+   NAME
+      ${SERVICENAME} - Runs Swagger
+   SYNOPSIS
+      ${SERVICENAME} start  - Launch Swagger
+      ${SERVICENAME} stop   - Stop Swagger
+      ${SERVICENAME} info   - Check status
+   ]]
+end
